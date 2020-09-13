287d6ae196a4b63df06f70dabc138192cf9a25d7
Juli Ramon <juli@nexumlabs.com>
2020-09-13 14:11:07 +0200

    fixed bug related to cookies

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -6,7 +6,7 @@ import GoogleLogin from "react-google-login";
 
 const Signup = (props) => {
 	const history = useHistory();
-	if (props.user !== "null" || !props.user) {
+	if (props.user && props.user !== "null") {
 		history.push("/feed");
 	}
 	const initialState = {
